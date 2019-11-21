2e2f7e0b73a6e6703b768493af8b060cabdb3bfc
eugenejeong1226 <eugene.jeong@cooper.edu>
2019-11-20 20:05:32 -0500

    I did it mom!

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import sys
-
+from statistics import mean
 
 def main():
     assert len(sys.argv) > 1, "No input file path specified."
 
     input_file_path = sys.argv[1]
     print(f"Processing input file: {input_file_path}")
+    with open(input_file_path, "r") as f:
+        for line in f:
+            daddy = line.split(",")
+            mommy = []
+            for n in daddy:
+                mommy.append(float(n))
 
-    # TODO: Fill in the actual logic here!
+            avg = mean(mommy)
+            print(f"{avg}")
+
+
+
+
 
 
 if __name__ == "__main__":
